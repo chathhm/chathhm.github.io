7858d074ffd97d6535e0edc63dd4621c79945e11
chathhm <118280037+chathhm@users.noreply.github.com>
2022-11-15 08:30:32 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp{
+firebase.initializeApp({
  apiKey: "AIzaSyDEJ31xnZMlER5JuCpll758hgI-gMayyWs",
   authDomain: "chathhm43.firebaseapp.com",
   projectId: "chathhm43",
@@ -9,5 +9,5 @@ firebase.initializeApp{
   messagingSenderId: "775335829921",
   appId: "1:775335829921:web:a6355c3e0c19e01ad62be0",
   measurementId: "G-5RKB2N55JN"
-};
+});
 
